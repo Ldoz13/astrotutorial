1d2b7313297391a2d47ce512ac29c5d44a76bea9
Eldhose Chackochan <eldhosechittamadathil@gmail.com>
2023-11-18 02:12:38 +0530

    Added Dark Mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astroproject-ldoz.netlify.app"
+  site: "https://astroproject-ldoz.netlify.app",
+  integrations: [preact()]
 });
